715e323f1ed9987ff739435dc56fcf92296274f3
Noah <noah.stoeckel@gmail.com>
2019-02-09 12:34:08 -0500

    Adds Components
    -Adds SmurfList Component
    -Adds Smurf Component
    -Adds fetchSmurfs action creator

---
+++ smurfs/src/components/SmurfList/Smurf.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Smurf = props => {
+  return (
+    <li>
+      <div>
+        <h4>{props.smurf.name}</h4>
+      </div>
+    </li>
+  );
+};
+
+export default Smurf;
